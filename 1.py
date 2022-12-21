23e03dd1736e5845d64d4ffbb1365f9f96beccd7
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-12-21 16:21:02 +0100

    finish day 1

--- 1.py
+++ 1.py
@@ -1,18 +1,18 @@
-dict = {}
-arr = []
-c = 0
-with open("data/1-data.txt") as file:
-    data = file.read()
-    for line in data.split('\n'):
-        if line.strip():
-          arr.append(line)
-        else:
-          dict[c] = sum(int(i) for i in arr)
-          arr.clear()
-          c += 1
+elves = []
+calories = 0
 
-print(max(dict.values()))
-print(sum(sorted(dict.values())[-3:]))
+with open("data/1.txt") as file:
+  for line in file:
+      # line not empty
+      if line.strip():
+        calories += int(line)
+      else:
+        elves.append(calories)
+        calories = 0
+
+
+print("Task 1:", max(elves))
+print("Task 2:", sum(sorted(elves)[-3:]))
 
 
 
