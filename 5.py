0d1a9f32486335b9b9ca289390b95b50643950fc
Isabelle Galleberg <isabellegalleberg@gmail.com>
2023-01-11 21:43:18 +0100

    finish day 5

--- 5.py
+++ 5.py
@@ -1,19 +1,22 @@
 tmpStore = []
-stacks = []
+stacks_1 = []
+stacks_2 = []
 
 with open('data/5.txt') as file:
   for line in file:
     # get number of stacks from file
     if line[1] == '1':
-      numStacks = int(line.split()[-1])
-      for stack in range(numStacks):
-        stacks.append([])
+      numCrates = int(line.split()[-1])
+      for stack in range(numCrates):
+        stacks_1.append([])
+        stacks_2.append([])
 
       # each stack is a list with crates
       for elem in tmpStore:
         for stack in range(1, len(elem), 4):
           if elem[stack].strip():
-            stacks[stack//4].append(elem[stack])
+            stacks_1[stack//4].append(elem[stack])
+            stacks_2[stack//4].append(elem[stack])
       break
 
     else:
@@ -24,17 +27,26 @@ with open('data/5.txt') as file:
   for line in file:
     if line.strip():
       line = line.split()
-      numStacks = int(line[1])
-      fromStack = stacks[int(line[3]) - 1]
-      toStack = stacks[int(line[5]) - 1]
+      numCrates = int(line[1])
+      fromStack = int(line[3]) - 1
+      toStack = int(line[5]) - 1
 
-      for stack in range(numStacks):
-        crate = fromStack.pop(0)
-        toStack.insert(0, crate)
+      # task 1
+      for stack in range(numCrates):
+        crate = stacks_1[fromStack].pop(0)
+        stacks_1[toStack].insert(0, crate)
 
-  print('Task 1:')
-  for stack in stacks:
-    print(stack[0])
+      # task 2
+      stacks_2[toStack] = stacks_2[fromStack][:numCrates] + stacks_2[toStack]
+      stacks_2[fromStack] = stacks_2[fromStack][numCrates:]
+
+  task1 = ""
+  task2 = ""
+  for stack in zip(stacks_1, stacks_2):
+    task1 += stack[0][0]
+    task2 += stack[1][0]
+  print('Task 1:', task1)
+  print('Task 2:', task2)
 
       
 
